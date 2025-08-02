8ec1a59cafd048032e8963cd36406b4cf4883ce1
Md. Sazzad Talukder <sazzadtalukdercseiu@gmail.com>
2025-08-02 14:03:07 +0600

    COmponent info added

---
+++ src/component/TestingComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestingComponent = () => {
+    return (
+        <div>
+            <p>This is Test Component</p>
+        </div>
+    );
+};
+
+export default TestingComponent;
